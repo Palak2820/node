210db32f8977544b7879c9e0949fb77941acff31
Palak2820 <54177848+Palak2820@users.noreply.github.com>
2019-09-08 19:24:02 +0530

    add

--- app/app.js
+++ app/app.js
@@ -1,14 +1,56 @@
-const http =require('http');
+const path = require('path');
 
-const express =require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
-app.use((req,res,next) => {
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-    console.log('In the middleware');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
-const server = http.createServer(app);
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-server.listen(3000);
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
